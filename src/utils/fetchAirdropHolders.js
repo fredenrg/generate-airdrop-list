6ea7e7cb7188e93ec05f3f1395c44116a01d3328
fredenrg <39649555+fredenrg@users.noreply.github.com>
2023-09-19 07:04:18 +0300

    Update fetchAirdropHolders.js
    Removed chaff

--- src/utils/fetchAirdropHolders.js
+++ src/utils/fetchAirdropHolders.js
@@ -18,42 +18,6 @@ const fetchMultipleHolders = async () => {
     }
     allHolders.push(...fracOfHolders.data._embedded.records);
   }
-  for (i = 1; i > -1; i++) {
-    const fracOfHolders = await axios.get(
-      `https://api.stellar.expert/explorer/public/asset/WHALE-GBPZTFUAQOIQ3LAIC6Q25SNGOE7TJ5SWJNMKN3ZU7HVQMONFXWAOQYWZ/holders?order=asc&cursor=${
-        i === 1 ? 0 : (i - 1) * 200
-      }&limit=${i * 200}`
-    );
-    if (fracOfHolders.data._embedded.records.length === 0) {
-      console.log(allHolders.length, "FRED + WHALE");
-      break;
-    }
-    allHolders.push(...fracOfHolders.data._embedded.records);
-  }
-  for (i = 1; i > -1; i++) {
-    const fracOfHolders = await axios.get(
-      `https://api.stellar.expert/explorer/public/asset/DODA-GBADNYBC3JYAA76GWTY4YVPVMEGZKAYPDM3DPURCIIWQHTLNGWFK73Z5/holders?order=asc&cursor=${
-        i === 1 ? 0 : (i - 1) * 200
-      }&limit=${i * 200}`
-    );
-    if (fracOfHolders.data._embedded.records.length === 0) {
-      console.log(allHolders.length, "FRED + WHALE + DODA");
-      break;
-    }
-    allHolders.push(...fracOfHolders.data._embedded.records);
-  }
-  for (i = 1; i > -1; i++) {
-    const fracOfHolders = await axios.get(
-      `https://api.stellar.expert/explorer/public/asset/UBEC-GDPNB7S3IOM2J6C3NA2QG4TQAUCRZXPJJ4HSCSIKELEH7ORUCX5UB2VN/holders?order=asc&cursor=${
-        i === 1 ? 0 : (i - 1) * 200
-      }&limit=${i * 200}`
-    );
-    if (fracOfHolders.data._embedded.records.length === 0) {
-      console.log(allHolders.length, "FRED + WHALE + DODA + UBEC");
-      break;
-    }
-    allHolders.push(...fracOfHolders.data._embedded.records);
-  }
   console.log("Total holders", allHolders.length);
   stringify(
     allHolders,
