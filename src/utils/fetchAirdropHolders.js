c89ad3b2c9cb06dea8cc729d9a7b6c163fe7bf6d
fredenrg <39649555+fredenrg@users.noreply.github.com>
2023-09-25 09:34:43 +0300

    Update fetchAirdropHolders.js
    Rewrite for paging_token

--- src/utils/fetchAirdropHolders.js
+++ src/utils/fetchAirdropHolders.js
@@ -1,37 +1,50 @@
-const fs = require("fs");
-const StellarSdk = require("stellar-sdk");
-const server = new StellarSdk.Server("https://horizon.stellar.org");
 const axios = require("axios");
 const { stringify } = require("csv-stringify");
+const fs = require("fs");
 
-const fetchMultipleHolders = async () => {
+async function fetchMultipleHolders() {
   const allHolders = [];
-  for (i = 1; i > -1; i++) {
-    const fracOfHolders = await axios.get(
-      `https://api.stellar.expert/explorer/public/asset/FRED-GCA73U2PZFWAXJSNVMEVPNPPJCZGETWPWZC6E4DJAIWP3ZW3BAGYZLV6/holders?order=asc&cursor=${
-        i === 1 ? 0 : (i - 1) * 200
-      }&limit=${i * 200}`
-    );
-    if (fracOfHolders.data._embedded.records.length === 0) {
-      console.log(allHolders.length, "FRED");
+  let nextCursor = ''; // Start with an empty string as cursor
+
+  while (true) {
+    try {
+      const url = `https://api.stellar.expert/explorer/public/asset/FRED-GCA73U2PZFWAXJSNVMEVPNPPJCZGETWPWZC6E4DJAIWP3ZW3BAGYZLV6/holders?order=asc&limit=200${nextCursor ? `&cursor=${nextCursor}` : ''}`;
+      const fracOfHolders = await axios.get(url);
+
+      if (fracOfHolders.data._embedded.records.length === 0) {
+        console.log(allHolders.length, "FRED");
+        break;
+      }
+
+      allHolders.push(...fracOfHolders.data._embedded.records);
+
+      // Check if there is a next page
+      if (!fracOfHolders.data._links.next) {
+        break;
+      }
+
+      // Update the cursor with the paging_token of the last record
+      nextCursor = allHolders[allHolders.length - 1].paging_token;
+    } catch (error) {
+      console.error("Error fetching holders", error);
       break;
     }
-    allHolders.push(...fracOfHolders.data._embedded.records);
   }
+
   console.log("Total holders", allHolders.length);
-  stringify(
-    allHolders,
-    {
-      header: true,
-    },
-    function (err, output) {
-      fs.writeFile(__dirname + "/airdrop-list.csv", output, (err, data) => {
+
+  stringify(allHolders, { header: true }, function (err, output) {
+    fs.writeFile(__dirname + "/airdrop-list.csv", output, (err, data) => {
+      if (err) {
+        console.error("Error writing to file", err);
+      } else {
         console.log("List generated!");
-      });
-    }
-  );
+      }
+    });
+  });
+
   return allHolders;
-};
+}
 
 module.exports = {
   fetchMultipleHolders,
